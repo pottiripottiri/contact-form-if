0dfdce7aa7e3ba99ee4769feeb2a979154400665
sk-plus <kaneyasu2405@gmail.com>
2021-08-25 01:23:57 +0900

    複数選択系フォーム（checkboxなど）における必須項目のユニットテストを実装

--- tests/test-wpcfif-validate.php
+++ tests/test-wpcfif-validate.php
@@ -19,25 +19,35 @@ class WPCFIFValidateGreaterTest extends WP_UnitTestCase {
 	 * テスト実行前処理
 	 * テストケースクラスの最初のテストメソッドの実行前に一度だけ実行
 	 */
-	public function setUp()
-    {
-        // WPCF7_Submissionがシングルトンのため、
+	public function setUp() {
+		// WPCF7_Submissionがシングルトンのため、
 		// テストケースごとにValidateかけてもうまくいかない
 		// ここで全テストケース分のValidateをかけて後で結果だけ見る
 
 		$form = <<<EOM
-[text eq-1][text eq-2][text eq-3][text eq-4][text eq-5]
+[text isnull-1][text isnull-2][text isnull-3][checkbox isnull-4 "1" "2" "3"][text isnull-5]
+[text notnull-1][text notnull-2][text notnull-3][checkbox notnull-4 "1" "2" "3"][text notnull-5]
+[text eq-1][text eq-2][text eq-3][text eq-4][text eq-5][checkbox eq-6 "1" "2" "3"][text eq-7][checkbox eq-8 "1" "2" "3"][text eq-9]
 [text greater-1][text greater-2][text greater-3][text greater-4][text greater-5]
 [text less-1][text less-2][text less-3][text less-4][text less-5]
 [text in-1][text in-2][text in-3][text in-4][text in-5]
+[checkbox in-6 "1" "2" "3" "4"][text in-7][text in-8][text in-9][text in-10]
+
 [submit "送信"]`
 EOM;
 
 		$additional_settings = <<<EOM
+requireif-isnull-2: isnull-1,is_null,
+requireif-isnull-3: isnull-1,is_null,
+requireif-isnull-5: isnull-4,is_null,
+requireif-notnull-2: notnull-1,not_null,
+requireif-notnull-3: notnull-1,not_null,
 requireif-eq-2: eq-1,equal,1
 requireif-eq-3: eq-1,equal,1
 requireif-eq-4: eq-1,equal,2
 requireif-eq-5: eq-1,not_equal,2
+requireif-eq-7: eq-6,equal,1
+requireif-eq-9: eq-8,not_equal,1
 requireif-greater-2: greater-1,greater_than,1
 requireif-greater-3: greater-1,greater_than,2
 requireif-greater-4: greater-1,greater_equal,1
@@ -47,66 +57,110 @@ requireif-less-3: less-1,less_than,1
 requireif-less-4: less-1,less_equal,2
 requireif-less-5: less-1,less_equal,1
 requireif-in-2: in-1,in,1 3
-requireif-in-3: in-1,in,1 2
-requireif-in-4: in-1,not_in,1 2
-requireif-in-5: in-1,not_in,2 3
+requireif-in-3: in-1,in,2 3
+requireif-in-4: in-1,not_in,2 3
+requireif-in-5: in-1,not_in,1 3
+requireif-in-7: in-6,in,1 3
+requireif-in-8: in-6,in,2 3
+requireif-in-9: in-6,not_in,2 3
+requireif-in-10: in-6,not_in,1 3
 EOM;
 
-		remove_all_filters('wpcfif_filter_input_post', 99);
-		add_filter( 'wpcfif_filter_input_post', function($name, $value) {
-			return array(
-				'eq-1' => 1,
-				'eq-2' => '',
-				'eq-3' => '1',
-				'eq-4' => '',
-				'eq-5' => '',
-				'greater-1' => 2,
-				'greater-2' => '',
-				'greater-3' => '',
-				'greater-4' => '',
-				'greater-5' => '',
-				'less-1' => 1,
-				'less-2' => '',
-				'less-3' => '',
-				'less-4' => '',
-				'less-5' => '',
-				'in-1' => 3,
-				'in-2' => '',
-				'in-3' => '',
-				'in-4' => '',
-				'in-5' => '',
-				)[$name];
-		}, 99, 2 );
+		remove_all_filters( 'wpcfif_filter_input_post', 99 );
+		add_filter(
+			'wpcfif_filter_input_post',
+			function( $name, $value ) {
+				return array(
+					'isnull-1'  => '',
+					'isnull-2'  => '',
+					'isnull-3'  => '1',
+					'isnull-4'  => array(),
+					'isnull-5'  => '',
+					'notnull-1' => '1',
+					'notnull-2' => '',
+					'notnull-3' => '1',
+					'notnull-4' => array( '1' ),
+					'notnull-5' => '',
+					'eq-4'      => '',
+					'eq-1'      => 1,
+					'eq-2'      => '',
+					'eq-3'      => '1',
+					'eq-4'      => '',
+					'eq-5'      => '',
+					'eq-6'      => array( '1' ),
+					'eq-7'      => '',
+					'eq-8'      => array( '2', '3' ),
+					'eq-9'      => '',
+					'greater-1' => 2,
+					'greater-2' => '',
+					'greater-3' => '',
+					'greater-4' => '',
+					'greater-5' => '',
+					'less-1'    => 1,
+					'less-2'    => '',
+					'less-3'    => '',
+					'less-4'    => '',
+					'less-5'    => '',
+					'in-1'      => 1,
+					'in-2'      => '',
+					'in-3'      => '',
+					'in-4'      => '',
+					'in-5'      => '',
+					'in-6'      => array( 1, 4 ),
+					'in-7'      => '',
+					'in-8'      => '',
+					'in-9'      => '',
+					'in-10'     => '',
+				)[ $name ];
+			},
+			99,
+			2
+		);
 
-		$id = $this->factory->post->create(array(
-			'post_type' => 'wpcf7_contact_form',
-			'post_content' => $form,
-			'meta_input' => array(
-				'form' => $form,
-				'additional_settings' => $additional_settings,
-			),
-		));
+		$id = $this->factory->post->create(
+			array(
+				'post_type'    => 'wpcf7_contact_form',
+				'post_content' => $form,
+				'meta_input'   => array(
+					'form'                => $form,
+					'additional_settings' => $additional_settings,
+				),
+			)
+		);
 
-		$WPCF = WPCF7_ContactForm::get_instance(get_post($id));
-		$Submission = WPCF7_Submission::get_instance($WPCF);
-		if (is_null($Submission)) {
+		$WPCF       = WPCF7_ContactForm::get_instance( get_post( $id ) );
+		$Submission = WPCF7_Submission::get_instance( $WPCF );
+		if ( is_null( $Submission ) ) {
 			$Submission = WPCF7_Submission::get_instance();
 		}
 		$this->invalid_fields = $Submission->get_invalid_fields();
 
 		return;
-    }
+	}
+
+	/**
+	 * テキスト項目のバリデーション（IS NULL、NOT NULL）.
+	 */
+	public function test_validate_text_is_null() {
+
+		$this->assertEquals( true, isset( $this->invalid_fields['isnull-2'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['isnull-3'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['notnull-2'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['notnull-3'] ) );
+
+	}
 
 	/**
 	 * テキスト項目のバリデーション（完全一致、不一致）.
 	 */
 	public function test_validate_text_equal() {
 
-		$this->assertEquals(true, isset($this->invalid_fields['eq-2']));
-		$this->assertEquals(false, isset($this->invalid_fields['eq-3']));
-		$this->assertEquals(false, isset($this->invalid_fields['eq-4']));
-		$this->assertEquals(true, isset($this->invalid_fields['eq-5']));
-
+		$this->assertEquals( true, isset( $this->invalid_fields['eq-2'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['eq-3'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['eq-4'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['eq-5'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['eq-7'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['eq-9'] ) );
 	}
 
 	/**
@@ -114,10 +168,10 @@ EOM;
 	 */
 	public function test_validate_text_greater() {
 
-		$this->assertEquals(true, isset($this->invalid_fields['greater-2']));
-		$this->assertEquals(false, isset($this->invalid_fields['greater-3']));
-		$this->assertEquals(true, isset($this->invalid_fields['greater-4']));
-		$this->assertEquals(true, isset($this->invalid_fields['greater-5']));
+		$this->assertEquals( true, isset( $this->invalid_fields['greater-2'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['greater-3'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['greater-4'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['greater-5'] ) );
 
 	}
 
@@ -126,10 +180,10 @@ EOM;
 	 */
 	public function test_validate_text_less() {
 
-		$this->assertEquals(true, isset($this->invalid_fields['less-2']));
-		$this->assertEquals(false, isset($this->invalid_fields['less-3']));
-		$this->assertEquals(true, isset($this->invalid_fields['less-4']));
-		$this->assertEquals(true, isset($this->invalid_fields['less-5']));
+		$this->assertEquals( true, isset( $this->invalid_fields['less-2'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['less-3'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['less-4'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['less-5'] ) );
 
 	}
 
@@ -138,11 +192,15 @@ EOM;
 	 */
 	public function test_validate_text_in() {
 
-		$this->assertEquals(true, isset($this->invalid_fields['in-2']));
-		$this->assertEquals(false, isset($this->invalid_fields['in-3']));
-		$this->assertEquals(true, isset($this->invalid_fields['in-4']));
-		$this->assertEquals(false, isset($this->invalid_fields['in-5']));
+		$this->assertEquals( true, isset( $this->invalid_fields['in-2'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['in-3'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['in-4'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['in-5'] ) );
 
+		$this->assertEquals( true, isset( $this->invalid_fields['in-7'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['in-8'] ) );
+		$this->assertEquals( true, isset( $this->invalid_fields['in-9'] ) );
+		$this->assertEquals( false, isset( $this->invalid_fields['in-10'] ) );
 	}
 
 }
--- tests/test-wpcfif.php
+++ tests/test-wpcfif.php
@@ -29,24 +29,26 @@ requireif-test-3: test-1,equal,2
 dummy: true
 EOM;
 
-		$id = $this->factory->post->create(array(
-			'post_type' => 'wpcf7_contact_form',
-			'post_content' => $form,
-			'meta_input' => array(
-				'form' => $form,
-				'additional_settings' => $additional_settings,
-			),
-		));
-		$WPCF = WPCF7_ContactForm::get_instance(get_post($id));
+		$id     = $this->factory->post->create(
+			array(
+				'post_type'    => 'wpcf7_contact_form',
+				'post_content' => $form,
+				'meta_input'   => array(
+					'form'                => $form,
+					'additional_settings' => $additional_settings,
+				),
+			)
+		);
+		$WPCF   = WPCF7_ContactForm::get_instance( get_post( $id ) );
 		$WPCFIF = WPCFIF::get_instance();
-		$this->assertEquals($additional_settings, $WPCFIF->get_properties()['additional_settings']);
+		$this->assertEquals( $additional_settings, $WPCFIF->get_properties()['additional_settings'] );
 	}
 
 	/**
 	 * WPCFのタグ情報を解析して、該当するポストデータが取得できること.
 	 */
 	public function test_get_post_value_from_wpcftag() {
-		
+
 		// POSTの値を取るのにモックを使ってみたけれどわかりにい
 		// また、フックを経由するとこの方法だと効かないので（無茶なことをすればできそうな気もするが・・・）、
 		// 自前のPOST値取得のラッピングメソッドにフックの口を設けて、
@@ -55,24 +57,92 @@ EOM;
 		// $Mock_WPCFIF = $this->createPartialMock(WPCFIF::class, array('filter_input_post'));
 		// $Mock_WPCFIF->method('filter_input_post')->willReturn(1);
 		// $tag = new WPCF7_FormTag(array(
-		// 	'type' => 'text',
-		// 	'basetype' => 'text',
-		// 	'name' => 'test-1'
+		// 'type' => 'text',
+		// 'basetype' => 'text',
+		// 'name' => 'test-1'
 		// ));
 		// $this->assertEquals('1', $Mock_WPCFIF->get_post_value($tag));
 
-		remove_all_filters('wpcfif_filter_input_post', 99);
-		add_filter( 'wpcfif_filter_input_post', function($name, $value) {
-			return 1;
-		}, 99, 2 );
+		remove_all_filters( 'wpcfif_filter_input_post', 99 );
+		add_filter(
+			'wpcfif_filter_input_post',
+			function( $name, $value ) {
+				return array(
+					'test-1'   => 1,
+					'select-1' => 1,
+					'multi-2'  => array( 1, 2 ),
+					'check-1'  => array( 1, 2 ),
+					'date-1'   => '2021-01-01',
+					'number-1' => 1,
+				)[ $name ];
+			},
+			99,
+			2
+		);
 
 		$WPCFIF = WPCFIF::get_instance();
-		$tag = new WPCF7_FormTag(array(
-			'type' => 'text',
-			'basetype' => 'text',
-			'name' => 'test-1'
-		));
-		$this->assertEquals('1', $WPCFIF->get_post_value($tag));
+		$this->assertEquals(
+			'1',
+			$WPCFIF->get_post_value(
+				new WPCF7_FormTag(
+					array(
+						'type'     => 'text',
+						'basetype' => 'text',
+						'name'     => 'test-1',
+					)
+				)
+			)
+		);
 
+		$WPCFIF = WPCFIF::get_instance();
+		$this->assertEquals(
+			'1',
+			$WPCFIF->get_post_value(
+				new WPCF7_FormTag(
+					array(
+						'type'     => 'select',
+						'basetype' => 'select',
+						'name'     => 'select-1',
+					)
+				)
+			)
+		);
+
+		$this->assertEquals(
+			array( 1, 2 ),
+			$WPCFIF->get_post_value(
+				new WPCF7_FormTag(
+					array(
+						'type'     => 'checkbox',
+						'basetype' => 'checkbox',
+						'name'     => 'check-1',
+					)
+				)
+			)
+		);
+		$this->assertEquals(
+			'2021-01-01',
+			$WPCFIF->get_post_value(
+				new WPCF7_FormTag(
+					array(
+						'type'     => 'date',
+						'basetype' => 'date',
+						'name'     => 'date-1',
+					)
+				)
+			)
+		);
+		$this->assertEquals(
+			'1',
+			$WPCFIF->get_post_value(
+				new WPCF7_FormTag(
+					array(
+						'type'     => 'number',
+						'basetype' => 'number',
+						'name'     => 'number-1',
+					)
+				)
+			)
+		);
 	}
 }
